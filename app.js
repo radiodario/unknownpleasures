f72ec575329a1065e52eb982fa289732fb40c4ba
Dario Villanueva Ablanedo <dario@alolo.co>
2023-04-19 14:45:44 +0100

    Updates

--- app.js
+++ app.js
@@ -511,6 +511,7 @@ canvas.width = w;
 var ctx = canvas.getContext('2d');
 var vid = document.createElement('video');
 vid.setAttribute('autoplay', true);
+vid.setAttribute('playsinline', "");
 var vidCan = document.createElement('canvas');
 vidCan.width = resolution;
 vidCan.height = verticalLines;
